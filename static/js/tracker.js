9fcdae461d695357664b1cbfa27a67a28c59e471
Denis <you@example.com>
2019-07-07 02:26:34 +1000

    Передача координат на сервер

--- static/js/tracker.js
+++ static/js/tracker.js
@@ -1,5 +1,34 @@
+function Socket() {
+	this.reconnect();
+}
+
+Socket.prototype = {
+	sendCoordinates: function (x, y) {
+		this.socket.send({x: x, y: y}, function (answer) {
+			console.log(answer)
+		});
+	},
+
+	reconnect: function () {
+		this.socket && this.socket.close();
+
+		this.socket = io('http://127.0.0.1:3000', {
+			transports: ['websocket']
+		});
+
+		this.socket.on('connect', function () {
+		});
+
+		this.socket.on('disconnect', function () {
+			this.reconnect();
+		}.bind(this));
+	}
+};
+
+
 $(function () {
 	var $tracker = $('.js-tracking-percents');
+	var socket = new Socket();
 
 	$tracker.on('mousemove', function (e) {
 		var offset = $tracker.offset();
@@ -12,6 +41,7 @@ $(function () {
 		var xPersent = (x / width).toFixed(6);
 		var yPersent = (y / height).toFixed(6);
 
-		console.log(xPersent, yPersent)
+
+		socket.sendCoordinates(xPersent, yPersent)
 	})
 });
